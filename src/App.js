99ee80937a9547de4ea9b66cbc800946777adbb8
FevenSeyfu <fevenseyfu00@gmail.com>
2023-05-06 17:54:33 +0300

    setup routes in App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from './components/Books';
+import Catagories from './components/Catagories';
+import NotFound from './components/NotFound';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+			<NavBar />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Books />} />
+          <Route path="/catagories" element={<Catagories />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
