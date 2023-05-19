e8a9ffb2f82b8b897742b0cf512676f33fbc8d33
FevenSeyfu <fevenseyfu00@gmail.com>
2023-05-19 21:36:35 +0300

    Remove old piece of code

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,19 +1,9 @@
-// import { useSelector } from 'react-redux';
 import BookCard from './BookCard';
 import Form from './AddNewBook';
 
 function Books() {
   return (
     <section className="ListBooks">
-      {/* {booksList.map((book) => (
-        <BookCard
-          key={book.item_id}
-          id={book.item_id}
-          category={book.category}
-          title={book.title}
-          author={book.author}
-        />
-      ))} */}
       <BookCard />
       <Form />
     </section>
