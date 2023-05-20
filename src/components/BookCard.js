bc1584b23e17f96bb1aea5e37eb0fc175b80c04f
FevenSeyfu <fevenseyfu00@gmail.com>
2023-05-20 19:24:55 +0300

    Style book list

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -20,22 +20,31 @@ const BookCard = () => {
   return (
     books.map((book) => (
       <div key={book.item_id} className="bookCard">
-        <h3 className="bookGenre">{book.category}</h3>
-        <h1 className="bookTitle">{book.title}</h1>
-        <h2 className="bookAuthor">{book.author}</h2>
-        <ul className="bookActions">
-          <li className="action">Comments</li>
-          <button
-            type="button"
-            className="action"
-            onClick={() => {
-              dispatch(removeBook(book.item_id));
-            }}
-          >
-            Remove
-          </button>
-          <li className="action">Edit</li>
-        </ul>
+        <div>
+          <h3 className="bookGenre">{book.category}</h3>
+          <h1 className="bookTitle">{book.title}</h1>
+          <h2 className="bookAuthor">{book.author}</h2>
+          <ul className="bookActions">
+            <li className="action">Comments</li>
+            <button
+              type="button"
+              className="action"
+              onClick={() => {
+                dispatch(removeBook(book.item_id));
+              }}
+            >
+              Remove
+            </button>
+            <li className="action last">Edit</li>
+          </ul>
+        </div>
+        <div className="progress-chart">
+          <div className="progress-bar"> </div>
+          <div className="progress-percentage">
+            <p className="percent">64%</p>
+            <p className="complete">completed</p>
+          </div>
+        </div>
         <div className="progress">
           <p className="current">CURRENT CHAPTER</p>
           <h3 className="chapterNumb">chapter</h3>
