ed76657a72782ffc2a2f9ee77555a54673e9430f
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-09 01:02:18 +0300

    make changes for deployment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import bookReducer from './books/bookSlice';
-import categoriesReducer from './categories/categoriesSlice';
+import bookReducer from './books/bookSlice.js';
+import categoriesReducer from './categories/categoriesSlice.js';
 
 const store = configureStore({
   reducer: {
